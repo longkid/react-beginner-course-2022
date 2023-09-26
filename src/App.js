b1cd6a0994f0fdb73b93bc70873f1d7dc6c78827
Lam Ho <hotienlam@gmail.com>
2023-09-26 17:21:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,90 @@
 import "./App.css";
+import { useState } from "react";
 
+// Episode 4: UseState Hook | States in React Tutorial
+// Tutorial: https://www.youtube.com/watch?v=iBfr6eOcCMg&list=PLpPqplz6dKxW5ZfERUPoYTtNUNvrEebAR&index=4
 function App() {
-  return <div className="App">Learn React</div>;
+  /*   // let age = 0;
+  const [age, setAge] = useState(0);
+
+  const increaseAge = () => {
+    // age = age + 1;
+    // console.log(age);
+    setAge(age + 1);
+  };
+
+  return (
+    <div className="App">
+      {age}
+      <button onClick={increaseAge}>Increase Age</button>
+    </div>
+  ); */
+
+  /* const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" onChange={handleInputChange} />
+      <p>{inputValue}</p>
+    </div>
+  ); */
+
+  /* const [showText, setShowText] = useState(true)
+
+  return (
+    <div>
+      <button onClick={() => {setShowText(!showText)}}>Show/Hide</button>
+      {showText && <h1>HI MY NAME IS PEDRO</h1>}
+    </div>
+  ) */
+
+  /* const [textColor, setTextColor] = useState("black");
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setTextColor(textColor === "black" ? "red" : "black");
+        }}
+      >
+        Change text color
+      </button>
+      {<h1 style={{ color: textColor }}>HI MY NAME IS PEDRO</h1>}
+    </div>
+  ); */
+
+  // Exercise
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+      <button
+        onClick={() => {
+          setCount(0);
+        }}
+      >
+        Set to Zero
+      </button>
+      <h1>{count}</h1>
+    </div>
+  );
 }
 
 export default App;
