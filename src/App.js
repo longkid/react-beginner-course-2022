3ac82b963367575f979e80537fb1fe07c0bddfaf
Lam Ho <hotienlam@gmail.com>
2023-09-27 11:24:45 +0700

    Fetch data from an API using Axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App">Learn React</div>;
+  // fetch("https://catfact.ninja/fact")
+  //   .then((res) => res.json())
+  //   .then((data) => console.log(data));
+  const [catFact, setCatFact] = useState("");
+
+  // Using axios to call API
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) =>
+      setCatFact(res.data.fact)
+    );
+  };
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+
+  return (
+    <div className="App">
+      <button onClick={() => fetchCatFact()}>Generate Cat Fact</button>
+      <p>{catFact}</p>
+    </div>
+  );
 }
 
 export default App;
