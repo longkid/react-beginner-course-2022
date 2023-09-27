d64874856b3db58829188dbdd80215e04650174d
Lam Ho <hotienlam@gmail.com>
2023-09-27 10:26:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { useState } from "react";
+import { Text } from "./Text";
 
 function App() {
-  return <div className="App">Learn React</div>;
+  const [showText, setShowText] = useState(false);
+
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          setShowText(!showText);
+        }}
+      >
+        Show Text
+      </button>
+
+      {showText && <Text />}
+    </div>
+  );
 }
 
 export default App;
