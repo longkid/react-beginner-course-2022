751c468437c88c988c34167b0078526e3b72a3ea
Lam Ho <hotienlam@gmail.com>
2023-10-04 15:26:37 +0700

    Prop drilling in React

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { Menu } from "./pages/Menu";
+import { Profile } from "./pages/Profile";
 import { Contact } from "./pages/Contact";
 import { Navbar } from "./Navbar";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("PedroTech");
+
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
+          <Route path="/" element={<Home username={username} />} />
+          <Route
+            path="/profile"
+            element={<Profile username={username} setUsername={setUsername} />}
+          />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
         </Routes>
