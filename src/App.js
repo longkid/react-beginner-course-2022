e4b44a4d8c5bddecde0f3a805e2e3ec8e6fe1bf9
Lam Ho <hotienlam@gmail.com>
2023-10-04 21:48:20 +0700

    React-Query: how to properly fetch data in React

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import { Home } from "./pages/Home";
 import { Menu } from "./pages/Menu";
 import { Contact } from "./pages/Contact";
 import { Navbar } from "./Navbar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const client = new QueryClient();
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
-        </Routes>
-      </Router>
+      <QueryClientProvider client={client}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import Axios from "axios";
+
 export const Home = () => {
-  return <h1>THIS IS THE HOME PAGE</h1>;
+  const {
+    data: catData,
+    isLoading,
+    refetch,
+  } = useQuery(["cat"], () => {
+    return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
+  });
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div>
+      <h1>THIS IS THE HOME PAGE</h1>
+      <p>{catData?.fact}</p>
+      <button onClick={() => refetch()}>Update data</button>
+    </div>
+  );
 };
