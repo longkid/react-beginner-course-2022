4b2fa6f85f2bf21bba89c1ec9a3773ebf1db0d9b
Lam Ho <hotienlam@gmail.com>
2023-10-06 12:27:32 +0700

    Create custom hook to fetch cat fact

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,10 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
-import Axios from "axios";
+import { useGetCat } from "../useGetCat";
 
 export const Cat = () => {
-  const { data: catData } = useQuery(["cat"], async () => {
-    return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
-  });
+  const { data, refetchData, isCatLoading } = useGetCat();
+  if (isCatLoading) return <h1>Loading...</h1>;
 
-  return <h1>{catData?.fact}</h1>;
+  return (
+    <div>
+      <button onClick={refetchData}>refetch</button>
+      <h1>{data?.fact}</h1>
+    </div>
+  );
 };
