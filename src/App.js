9c051547e0e471ba8c6135c0216ad93c208e5489
Lam Ho <hotienlam@gmail.com>
2023-10-06 12:37:43 +0700

    Create custom hook for counting numbers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
-import { Cat } from "./components/Cat";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useCount } from "./useCount";
 
 function App() {
-  const client = new QueryClient();
+  const { count, increase, decrease, reset } = useCount();
+
   return (
     <div className="App">
-      <QueryClientProvider client={client}>
-        <Cat />
-      </QueryClientProvider>
+      {count}
+      <button onClick={increase}>increase</button>
+      <button onClick={decrease}>decrease</button>
+      <button onClick={reset}>reset</button>
     </div>
   );
 }
