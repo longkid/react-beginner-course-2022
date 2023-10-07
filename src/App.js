28bc0f15785c4542d77ad2fd13d3bb0ac09faa6b
Lam Ho <hotienlam@gmail.com>
2023-10-07 10:58:42 +0700

    Use prop-types to define type of props properties

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Person } from "./components/Person";
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <div className="App">
+      <Person
+        name="Pedro"
+        email="pedro@gmail.com"
+        age={21}
+        isMarried={true}
+        friends={["Jessica", "Jake", "Jerry", "Jasmine"]}
+      />
+    </div>
+  );
 }
 
 export default App;
